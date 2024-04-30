de6c9c422dc2891084ae3953861f917e896c2532
posop <artur.yakhin52@gmail.com>
2024-04-30 23:33:22 +0500

    feat: add i18n

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import './assets/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
+import en from './locales/en.json'
+import ru from './locales/ru.json'
 
 const app = createApp(App)
 
+const i18n = createI18n({
+  locale: 'en',
+  messages: {
+    en,
+    ru
+  }
+})
+
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>asd</main>
+  <main>
+    <h1>{{ $t('hello') }}</h1>
+  </main>
 </template>
